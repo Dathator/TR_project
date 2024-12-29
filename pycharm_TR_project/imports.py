f24ab68b6cf6a5a4331749d5d4c2e1f0fb3bee68
Artur <61884137+Dathator@users.noreply.github.com>
2024-12-29 11:42:32 +0100

    2_MainAndImpports
    Adding imports and main file.

---
+++ pycharm_TR_project/imports.py
@@ -0,0 +1,5 @@
+from sys import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from sqlite3 import *
---
+++ pycharm_TR_project/main.py
@@ -0,0 +1,7 @@
+from imports import *
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    exit(app.exec())
